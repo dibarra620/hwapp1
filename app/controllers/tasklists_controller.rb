3e5e272cd017aef2538853d88e2ab41af302c697
dibarra <dibarra620@gmail.com>
2013-10-06 04:09:59 -0400

    small updates

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,5 +1,6 @@
 class TasklistsController < ApplicationController
   def index
+    @task_list = Tasklist.all
   end
 
   def new
@@ -13,7 +14,7 @@ class TasklistsController < ApplicationController
   end
 
   def create
-  	@task_list = Tasklist.new(params[:tasklast])
+  	@task_list = Tasklist.new(params[:tasklist])
   	if @task_list.save
   		redirect_to tasklist_path
   	else
